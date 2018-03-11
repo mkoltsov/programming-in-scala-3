d0a2035ba35c322cfe09f9541c0fe7e622e06ed5
mkoltsov <root@javabean.ru>
2018-03-11 21:28:41 +0100

    correction

--- fib.scala
+++ fib.scala
@@ -1,10 +1,11 @@
 val n = scala.io.StdIn.readInt
 
 var fubNum=new Array[Int](n+1)
-fubNum(0)=1
+fubNum(0)=0
 fubNum(1)=1
-for (i<-2 to n) {
+fubNum(2)=1
+for (i<-3 to n) {
     fubNum.update(i, (fubNum(i-1) + fubNum(i-2)))
 }
 
-println(fubNum(n-1))
+println(fubNum(n))
